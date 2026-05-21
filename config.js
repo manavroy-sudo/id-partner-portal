0ebc4efa0b8b90e6e6b545b72f48812a52c7b70d
manavroy-sudo <manav.roy@insurancedekho.com>
2026-05-21 15:46:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // STEP: After deploying Code.gs, paste your Apps Script Web App URL below
 // =====================================================================
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbwnM3rENVkxBQuxc_hiXbaKaOYLUK3YlfXqhmVLrW95Jw_rZuDgZkQP3fiR98j_jrIsYA/exec';
+const API_URL = 'https://script.google.com/macros/s/AkfycbwnM3rENVkxBQuxc_hiXbaKaOYLUK3YlfXqhmVLrW95Jw_rZuDgZkQP3fiR98j_jrIsYA/exec';
 
 // Example:
 // const API_URL = 'https://script.google.com/macros/s/AKfycby.../exec';
